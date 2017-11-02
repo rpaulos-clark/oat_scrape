cb66cdb7f15973d9fc91e640fdbd7c6250be8006
Ryan <rpaulos@clark.edu>
2017-11-02 09:49:17 -0700

    fnx for extracting newline courses

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import string
 
-# If there is a course but no outcomes, there is no entry (it seems..)
+# If there is a course but no outcomes, there is no entry that is downloaded(it seems..). So the two extraction rules
+# should be sufficient.
 # PROBLEM!!! Not all Course names and their outcomes are separated by a block of integers. Looks like second pattern
 # of newlines and integers will need solving
 integer_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
@@ -50,7 +51,32 @@ def extract_course_and_outcomes(tag_text):
         entry_list.append(string_tuple[0].strip())
     return entry_list
 
+def extract_with_newlines(string):
 
+
+    i = 0
+    k=1
+    slice_start = 0
+    flag = True
+    while i < len(string) and flag:
+        # print('main loop')
+        # print(string[i])
+        # print(i)
+        # print(k)
+        if string[i] == '\n':
+            if i+k < len(string):
+                if string[i+k] =='\n':
+                    k += 1
+                else:
+                    slice_start = i+k
+                    i = i + k
+                    flag = False
+            else:
+                return False # If the string is comprised entirely of newlines, will return False
+    while string[i] != '\n':
+        i += 1
+
+    return string[slice_start:i], string[i:]
 
 
 if __name__ == "__main__":
@@ -58,7 +84,7 @@ if __name__ == "__main__":
 
     # Save toolbox page with relevant department selected
     req = urllib.request.Request(
-        "file:///C:/Users/rpaulos/Desktop/Outcomes%20Assessment%20Toolbox_files/PTA.html",
+        "file:///C:/Users/rpaulos/Desktop/Outcomes%20Assessment%20Toolbox_files/MATH.html",
         data=None,
         # headers={
         #     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
@@ -82,7 +108,7 @@ if __name__ == "__main__":
 
 
 
-    print(page_soup)
+    #print(page_soup)
     # Below stores resultset in tag and is iterable. objects held in tag can be cast to string. Need to extract info.
     ################################
     tag = page_soup.find_all('tbody')
@@ -92,7 +118,10 @@ if __name__ == "__main__":
     #print(type(test))
     #print(len(test))
     #print(test)
-    print(tag)
+   # print(tag)
+
+
+
 
 
 
@@ -100,11 +129,13 @@ if __name__ == "__main__":
     #alphabet = extract_course_and_outcomes(test)
     #print(alphabet)
     j = 0
-    print(repr(tag[0].get_text()))
-    while j < len(tag):
-        test = tag[j].get_text()
-        #print(j, extract_course_and_outcomes(test))
-        #if len(extract_course_and_outcomes(test)) == 1:
-            #print(j, extract_course_and_outcomes(test))
-        j += 1
+    #print(repr(tag[0].get_text()))
+
+
+    # while j < len(tag):
+    #     test = tag[j].get_text()
+    #     #print(j, extract_course_and_outcomes(test))
+    #     #if len(extract_course_and_outcomes(test)) == 1:
+    #         #print(j, extract_course_and_outcomes(test))
+    #     j += 1
 
