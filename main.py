414740d5b814198784eb481c47ad858e9487c576
Ryan <rpaulos@clark.edu>
2017-11-02 11:03:59 -0700

    Working newline stripper woooooo

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import string
 
-# If there is a course but no outcomes, there is no entry that is downloaded(it seems..). So the two extraction rules
-# should be sufficient.
-# PROBLEM!!! Not all Course names and their outcomes are separated by a block of integers. Looks like second pattern
-# of newlines and integers will need solving
+# Newline Extraction Seems to be working dandy. Will need to screen for empty strings and those comprised exclusively of integers
+#
 integer_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 
@@ -17,7 +15,9 @@ def detect_consecutive_integers(string):
     # Returns a tuple
     # Detects 4 or more consecutive integers. If detected, will return the slice preceeding the consecutive integers
     # and the slice following the consecutive integers. If no set of 4+ integers are detected, it will return the
-    # original string and False
+    # original string and
+
+    # Working to make obsolete 11/2/2017
 
     i = 0
     consecutive_ints = 0
@@ -41,6 +41,7 @@ def extract_course_and_outcomes(tag_text):
 # takes in one of the beautiful soup4 tagResultSet cast to a string. This is fed into detect_consecutive_integers to
 # return the string slice preceeding the integer set and the slice after. This occurs until there are no more integer
 # sets to remove. A list of the returned string slices is then returned
+# Working to make obsolete. Will be template for newline version 11/2/2017
 #
     entry_list = []
     string_tuple = detect_consecutive_integers(tag_text)
@@ -51,32 +52,48 @@ def extract_course_and_outcomes(tag_text):
         entry_list.append(string_tuple[0].strip())
     return entry_list
 
-def extract_with_newlines(string):
+def detect_newlines(string):
 
+    # Takes in string. Begins first slice after last consecutive newline and ends first slice before next newline.
+    # Returns the remainder of the string at the most recently detected newline.  Returns False if the initial string
+    # is entirely newlines
 
     i = 0
     k=1
     slice_start = 0
     flag = True
     while i < len(string) and flag:
-        # print('main loop')
-        # print(string[i])
-        # print(i)
-        # print(k)
-        if string[i] == '\n':
+        if string[i] == '\n': # Begins walking out from first detected newline
             if i+k < len(string):
-                if string[i+k] =='\n':
+                if string[i+k] =='\n':  # Checking if next character is a newline
                     k += 1
-                else:
+                else:     # Character is not a newline, so mark beginning of slice and set flag to break loop
                     slice_start = i+k
                     i = i + k
                     flag = False
             else:
-                return False # If the string is comprised entirely of newlines, will return False
+                return string, False # If the string is comprised entirely of newlines, will return original string and False
+        else:   #string does not begin with a newline
+            slice_start = i
+            flag = False
     while string[i] != '\n':
         i += 1
 
     return string[slice_start:i], string[i:]
+
+
+def remove_newlines(tag_text):
+
+    # Seems to be working
+
+    entry_list = []
+    string_tuple = detect_newlines(tag_text)
+    entry_list.append(string_tuple[0].strip())
+
+    while string_tuple[1]:
+        string_tuple = detect_newlines(string_tuple[1])
+        entry_list.append(string_tuple[0].strip())
+    return entry_list
 
 
 if __name__ == "__main__":
@@ -114,17 +131,21 @@ if __name__ == "__main__":
     tag = page_soup.find_all('tbody')
     # print(len(tag))
     # print(tag[1])
-    #test = tag[12].get_text()
+    test = tag[12].get_text()
     #print(type(test))
     #print(len(test))
     #print(test)
    # print(tag)
 
+    z = 0
 
 
+    while z < len(tag):
+        print(remove_newlines(tag[z].get_text()))
+        z+=1
 
-
-
+    #alphabet = extract_course_and_outcomes(cheese)
+    #print(alphabet)
     #print(test)
     #alphabet = extract_course_and_outcomes(test)
     #print(alphabet)
@@ -132,10 +153,15 @@ if __name__ == "__main__":
     #print(repr(tag[0].get_text()))
 
 
+
+
+
+
     # while j < len(tag):
     #     test = tag[j].get_text()
-    #     #print(j, extract_course_and_outcomes(test))
-    #     #if len(extract_course_and_outcomes(test)) == 1:
-    #         #print(j, extract_course_and_outcomes(test))
+    #     temp.append(test)
+    #     print(j, extract_course_and_outcomes(test))
+    #     if len(extract_course_and_outcomes(test)) == 1:
+    #         print(j, extract_course_and_outcomes(test))
     #     j += 1
 
